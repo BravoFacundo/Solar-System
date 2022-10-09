d2aaa153893e9d5f88281c7a162ada936190219f
Facundo Bravo <facundobravot@gmail.com>
2022-10-09 19:42:31 -0300

    0.0.2
    - Instantiated sun and planets
    - Give sun and planets its values

--- Assets/Scripts/SolarSystemManager.cs
+++ Assets/Scripts/SolarSystemManager.cs
@@ -6,8 +6,13 @@ public class SolarSystemManager : MonoBehaviour
 {
     [Header("Configuration")]
     [SerializeField] private int planetCount;
+    public bool trails;
+
+    [Header("System Conditions")]
     public bool blackHole;
+    public bool planetColitions;
     public bool life;
+
 
     [Header("References")]
     [SerializeField] private List<GameObject> planets; 
@@ -19,18 +24,43 @@ public class SolarSystemManager : MonoBehaviour
 
     void Start()
     {
-        //Generar el sol, tamaño, color, ubicar en 0, es black hole?
+        GameObject newSun = Instantiate(planet, transform);
+        newSun.transform.parent = null; 
+        newSun.transform.position = Vector3.zero;
+        float newSunScale = Random.Range(0.1f, 6f);
+        newSun.transform.localScale *= newSunScale; // = new Vector3(scale, scale, scale);
+        newSun.name = GenerateName();
+        //Falta color, black hole?
 
-        //Generate planets
-        //For Loop, planetCount
-        //Intanciar prefab de planeta, guardar la referencia dentro del loop
-        //Agregar planeta a la lista de planetas
+        for (int i = 0; i < planetCount; i++)
+        {
+            GameObject newPlanet = Instantiate(planet, transform);
+            newPlanet.transform.parent = null;
+            newPlanet.transform.position = new Vector3(0,0, newSunScale/2 + Random.Range(1f, 10f));
+            float newPlanetScale = Random.Range(0.1f, 1f);
+            newPlanet.transform.localScale *= newPlanetScale;
+
+            newPlanet.name = GenerateName();
+            planets.Add(newPlanet);
+        }
+        //Falta color, rotacion inicial, cantidad de lunas, etc.
         //Instanciar objeto trail y hacerlo hijo del planeta.
-        //Modificar valores del planeta, distancia al sol, tamaño, rotacion inicial, cantidad de lunas,etc.
     }
 
     void Update()
     {
         
+    }
+
+    string GenerateName()
+    {
+        string randomName = "";
+        string characters = "abcdefghijkmnopqrstuvwxyz0123456789";
+        for (int i = 0; i < Random.Range(3, 10); i++)
+        {
+            randomName += characters[Random.Range(0, characters.Length)];
+        }
+
+        return randomName;
     }
 }
